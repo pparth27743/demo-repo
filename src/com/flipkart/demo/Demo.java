54e9724371b6de679a99d296293a2eae7ed511fb
pparth27743 <pparth27743@gmail.com>
2022-01-18 11:44:13 +0530

    my first commit

---
+++ src/com/flipkart/demo/Demo.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.flipkart.demo;
+
+/**
+ * @author parth
+ *
+ */
+public class Demo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		 System.out.println("Hello World");
+		 
+
+	}
+
+}
